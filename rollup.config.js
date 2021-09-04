bfe065ec883b0450a61922e2bbf248ecc22d0b5a
cuixiaorui <cui_xiaorui@126.com>
2021-09-04 22:18:37 +0800

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+import typescript from "@rollup/plugin-typescript";
+export default {
+  input: "src/main.ts",
+  output: {
+    file: "dist/bundle.js",
+    format: "iife",
+  },
+  plugins: [typescript()],
+};
