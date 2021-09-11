cd7d7e89630283a4baf2f7ca958ca050a3e74a0b
cuixiaorui <cui_xiaorui@126.com>
2021-09-11 21:17:20 +0800

    refactor: parse test

--- tests/parseProg.spec.ts
+++ tests/parseProg.spec.ts
@@ -1,46 +1,30 @@
 import { parseProg } from "../src/parseProg";
 import { createTokenizer } from "../src/Tokenizer";
-import { Token, TokenKind } from "../src/token";
+import { parseToken, Token, TokenKind } from "../src/token";
 
 describe("parseProg", () => {
-  it("parseFunctionBody happy path", () => {
-    const tokenArray: Token[] = [
-      { kind: TokenKind.Keyword, text: "function" },
-      { kind: TokenKind.Identifier, text: "sayHello" },
-      { kind: TokenKind.Separator, text: "(" },
-      { kind: TokenKind.Separator, text: ")" },
-      { kind: TokenKind.Separator, text: "{" },
-      { kind: TokenKind.Identifier, text: "println" },
-      { kind: TokenKind.Separator, text: "(" },
-      { kind: TokenKind.StringLiteral, text: "Hello" },
-      { kind: TokenKind.Separator, text: "," },
-      { kind: TokenKind.StringLiteral, text: "World!" },
-      { kind: TokenKind.Separator, text: ")" },
-      { kind: TokenKind.Separator, text: ";" },
-      { kind: TokenKind.Separator, text: "}" },
-      { kind: TokenKind.EOF, text: "" },
-    ];
-    const prog = parseProg(createTokenizer(tokenArray));
-  });
+  it("happy path", () => {
+    const progString = `function sayHello(){
+	      println("Hello","World!");
+    } 
 
-  it("parseFunctionBody have two functionCall", () => {
-    const tokenArray: Token[] = [
-      { kind: TokenKind.Keyword, text: "function" },
-      { kind: TokenKind.Identifier, text: "sayHello" },
-      { kind: TokenKind.Separator, text: "(" },
-      { kind: TokenKind.Separator, text: ")" },
-      { kind: TokenKind.Separator, text: "{" },
-      { kind: TokenKind.Identifier, text: "println" },
-      { kind: TokenKind.Separator, text: "(" },
-      { kind: TokenKind.Separator, text: ")" },
-      { kind: TokenKind.Separator, text: ";" },
-      { kind: TokenKind.Identifier, text: "println" },
-      { kind: TokenKind.Separator, text: "(" },
-      { kind: TokenKind.Separator, text: ")" },
-      { kind: TokenKind.Separator, text: ";" },
-      { kind: TokenKind.Separator, text: "}" },
-      { kind: TokenKind.EOF, text: "" },
-    ];
-    const prog = parseProg(createTokenizer(tokenArray));
+    sayHello();
+    `;
+    const tokens = parseToken(progString);
+    const prog = parseProg(createTokenizer(tokens));
+    expect(prog.stmts[0]).toEqual({
+      type: "functionDecl",
+      name: "sayHello",
+      functionBody: {
+        type: "functionBody",
+        stats: [
+          {
+            type: "functionCall",
+            name: "println",
+            params: ["Hello", "World!"],
+          },
+        ],
+      },
+    });
   });
 });
